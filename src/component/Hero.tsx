137ef686b13c7fd641faffcd4ca84640099572f1
wwwam <amannakoti0@gmail.com>
2025-10-02 22:43:42 +0530

    hero

--- src/component/Hero.tsx
+++ src/component/Hero.tsx
@@ -1,7 +1,10 @@
 export default function Hero() {
   return (
-    <div> 
-      Hero Section
+    <div className="hero ml-14 flex  h-screen bg-[#1E1E1E]"> 
+      <div className="flex align-center m-auto">
+        <h1 className="text-3xl text-white font-fira-condensed ">Welcome to My Portfolio</h1>
+                <h1 className="text-3xl text-white font-fira-condensed ">Welcome to My Portfolio</h1>
+      </div>
     </div>
   );
 }
