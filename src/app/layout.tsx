1e04b46bbcfe497091a7977c0e0d96e20ccae1e3
wwwam <amannakoti0@gmail.com>
2025-10-28 12:00:32 +0530

    RightClick Disable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ClientWrapper from "../component/ClientWrapper"; 
 import CustomCursor from "../component/CustomCursor";
+import DisableClicks from "../component/DisableClick";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
@@ -17,6 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ClientWrapper>
           <CustomCursor />
+          <DisableClicks />
           {children}
         </ClientWrapper>
       </body>
