58abd0c855199732a0074520d8eee85cf4d2a467
wwwam <amannakoti0@gmail.com>
2026-07-05 14:26:40 +0530

    favicon add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -106,11 +106,6 @@ export const metadata: Metadata = {
       "max-snippet": -1,
     },
   },
-
-  icons: {
-    icon: "./favicon.ico",
-    shortcut: "./favicon.ico",
-  },
 };
 
 const jsonLd = {
